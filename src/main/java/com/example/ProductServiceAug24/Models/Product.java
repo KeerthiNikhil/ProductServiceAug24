fa01b8d95dd9bd30faf5db5edebcf54f1dbb639f
KeerthiNikil <keerthubelchada@gmail.com>
2024-09-04 01:39:19 +0530

    Integration with 3rd Party API

---
+++ src/main/java/com/example/ProductServiceAug24/Models/Product.java
@@ -0,0 +1,32 @@
+package com.example.ProductServiceAug24.Models;
+
+
+public class Product {
+    String name;
+    String category;
+    String description;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
